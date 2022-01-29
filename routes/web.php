f8c47439ee4fb9a897205fcacfd194df238d0c28
setiyawan12 <yayangsetiyawan0@gmail.com>
2022-01-30 00:44:17 +0700

    5.roite

--- routes/web.php
+++ routes/web.php
@@ -16,30 +16,6 @@ use Illuminate\Support\Facades\Storage;
 |
 */
 
-Route::get('/', function () {
-    return redirect('/testing');
-    // $files = Storage::disk("google")->allfiles();
-    // dump($files);
-    // $firstFileName = $files[4];
-    // $array = array($firstFileName);
-    // dump("FILE NAME: " . $firstFileName);
-    // dump($array);
-    // $details = Storage::disk('google')->getMetadata($firstFileName);
-    // dump($details);
-    // $url = Storage::disk('google')->url($firstFileName);
-    // dump("Download URL (Session based): ");
-    // dump($url);
-});
-Route::post('/upload', function (Request $request) {
-    // $data = $request ->file("thing")->store("","google");
-    // $details = Storage::disk('google')->getMetadata($data);
-    // $value = array_values($details);
-    // $getvalue = $value[2];
-    // $url = Storage::disk('google')->url($getvalue);
-    // dump($url);
-});
-
-Route::get('/data','dataController@index')->name('index');
-Route::get('/tambahData','dataController@create')->name('tambah');
+Route::get('/', 'uploadController@index')->name('testing');
 Route::get('/testing', 'uploadController@index')->name('testing');
 Route::post('/uploadfile','uploadController@create')->name('uploadfile');
