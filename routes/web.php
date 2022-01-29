8174a8e4d5b85a72f986e4d79b5df8795f0a7a8c
setiyawan12 <yayangsetiyawan0@gmail.com>
2022-01-29 22:52:38 +0700

    get url drive

--- routes/web.php
+++ routes/web.php
@@ -27,41 +27,14 @@ Route::get('/', function () {
     // $url = Storage::disk('google')->url($firstFileName);
     // dump("Download URL (Session based): ");
     // dump($url);
-    // dump("FILE NAME: ||" . $firstFileName ."||". $url);
-    // $files = Storage::disk("google")->allfiles();
-    // dump($files);
-    // $firstFileName = $files[3];
-    // dump($firstFileName);
-    // $url = Storage::disk("google")->url($firstFileName);
-    // dump($url);
 });
 Route::post('/upload', function (Request $request) {
-    // dd($request->all());
     $data = $request ->file("thing")->store("","google");
     $details = Storage::disk('google')->getMetadata($data);
-    
-    dump($details);
-    $array = array($details);
-    $firstFileName = $array[0];
-    dump($array);
-    dump($firstFileName);
-    $url = Storage::disk('google')->url($firstFileName);
+    $value = array_values($details);
+    $getvalue = $value[2];
+    $url = Storage::disk('google')->url($getvalue);
     dump($url);
-
-    // dd($request->file("thing")->store("", "google"));
-    // // dump($data);
-    // $url = Storage::disk('google')->url($details);
-    // dump($url);
-    // $files = Storage::disk("google")->files();
-    // dump($files);
-    // $details = Storage::disk('google')->getMetadata($firstFileName);
-    // dump($details)
-    // $url = Storage::disk('google')->url($data);
-    // dump(url);
-    // $firstFileName = $files->$data;
-    // dump($firstFileName);
-
-
 });
 
 Route::get('/data','dataController@index')->name('index');
